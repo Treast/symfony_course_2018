f09fe8ec5ed80b9dbaec58745d1c9d0b4a4236e3
Vincent Riva <treast@MacBook-Pro-de-Vincent.local>
2018-11-19 11:35:15 +0100

    Add routes on MovieController

--- src/Migrations/Version20181119102250.php
+++ src/Migrations/Version20181119102250.php
@@ -15,7 +15,7 @@ final class Version20181119102250 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, movie VARCHAR(255) NOT NULL, genre VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, year INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, genre VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, year INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
