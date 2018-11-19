5ad812f8a0e548bcdc0cf0fd828e6de1b1acd4aa
Vincent Riva <treast@MacBook-Pro-de-Vincent.local>
2018-11-19 11:51:44 +0100

    Add create method on User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,13 +5,30 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\MovieRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
     public function index(UserRepository $userRepository) {
         $movies = $userRepository->findAll();
         return $this->json($movies);
+    }
+
+    public function create(EntityManagerInterface $entityManager, Request $request) {
+        $user = new User();
+
+        $data = json_decode($request->getContent());
+
+        $user->setUsername($data->username);
+        $user->setPassword($data->password);
+        $user->setEmail($data->email);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json($user);
     }
 
     public function show(UserRepository $userRepository, User $user) {
